c2eb10635c967624686785f6c4f44404fc5a374b
cbpark <cbpark@gmail.com>
2020-02-18 17:42:38 +0900

    Simple tries

--- src/runsushi.hs
+++ src/runsushi.hs
@@ -1,12 +1,25 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
-import qualified Data.Text          as T
-import qualified Data.Text.IO       as TIO
+import           Data.Double.Conversion.Text
+import qualified Data.Text                   as T
+import qualified Data.Text.IO                as TIO
 
-import           System.Environment (getArgs)
+import           System.Environment          (getArgs)
 
 main :: IO ()
 main = do
     infile <- head <$> getArgs
-    str <- TIO.readFile infile
-    mapM_ TIO.putStrLn $ take 3 (T.lines str)
+    str <- T.lines <$> TIO.readFile infile
+    let str' = map (replaceTYPE . replaceECM) (take 15 str)
+    mapM_ TIO.putStrLn str'
+  where
+    replaceECM  = T.replace "$ECM"  (toFixed 1 eCM)
+    replaceTYPE = T.replace "$TYPE" (T.pack (show mType))
+
+eCM :: Double
+eCM = 1.30e+4
+
+mType :: Int
+mType = 2
