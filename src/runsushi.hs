0d6f4129481ca69c42397ae55ae38b2a239cc7b5
cbpark <cbpark@gmail.com>
2020-02-18 17:03:35 +0900

    Tests for text

--- src/runsushi.hs
+++ src/runsushi.hs
@@ -1,4 +1,12 @@
 module Main where
 
+import qualified Data.Text          as T
+import qualified Data.Text.IO       as TIO
+
+import           System.Environment (getArgs)
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+    infile <- head <$> getArgs
+    str <- TIO.readFile infile
+    mapM_ TIO.putStrLn $ take 3 (T.lines str)
