8413d202aebe720b4a40cc9b4d98a86f8f1cfcff
cbpark <cbpark@gmail.com>
2020-02-18 21:52:47 +0900

    More tries

--- src/runsushi.hs
+++ src/runsushi.hs
@@ -12,14 +12,21 @@ main :: IO ()
 main = do
     infile <- head <$> getArgs
     str <- T.lines <$> TIO.readFile infile
-    let str' = map (replaceTYPE . replaceECM) (take 15 str)
+    let str' = map (replaceSinBA
+                    . replaceMCH
+                    . replaceMA
+                    . replaceMH
+                    . replaceM12
+                    . replaceTanb
+                    . replaceTYPE
+                    . replaceECM) str -- (take 25 str)
     mapM_ TIO.putStrLn str'
   where
-    replaceECM  = T.replace "$ECM"  (toFixed 1 eCM)
-    replaceTYPE = T.replace "$TYPE" (T.pack (show mType))
-
-eCM :: Double
-eCM = 1.30e+4
-
-mType :: Int
-mType = 2
+    replaceECM   = T.replace "$ECM"     (toFixed 1 1.30e+4)
+    replaceTYPE  = T.replace "$TYPE"    (T.pack (show (2 :: Int)))
+    replaceTanb  = T.replace "$TANBETA" (toFixed 1 2)
+    replaceM12   = T.replace "$M12"     (toExponential 7 50)
+    replaceMH    = T.replace "$MH"      (toExponential 7 500)
+    replaceMA    = T.replace "$MA"      (toExponential 7 500)
+    replaceMCH   = T.replace "$MCH"     (toExponential 7 300)
+    replaceSinBA = T.replace "$SINBA"   (toExponential 7 $ sqrt (1 - 0.1 ** 2))
