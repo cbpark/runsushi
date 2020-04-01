877a3c74b849491e1c06adc220319c1755122d4c
cbpark <cbpark@gmail.com>
2020-04-01 18:07:08 +0900

    Add optparse-generic

--- src/runsushi.hs
+++ src/runsushi.hs
@@ -1,26 +1,37 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DataKinds          #-}
+{-# LANGUAGE DeriveGeneric      #-}
+{-# LANGUAGE FlexibleInstances  #-}
+{-# LANGUAGE OverloadedStrings  #-}
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE TypeOperators      #-}
 
 module Main where
+
+import           Data.HEP.SLHA
 
 import           Data.Double.Conversion.Text
 import qualified Data.Text                   as T
 import qualified Data.Text.IO                as TIO
+import           Options.Generic
 import           System.Directory
 import           System.Process              (readProcess)
 
-import           Data.HEP.SLHA
-
 import           Control.Monad               (unless)
+-- import           Data.Maybe                  (fromMaybe)
 import           System.Environment          (getArgs)
 import           System.Exit                 (die)
 import           System.IO                   (IOMode (..), withFile)
 
 main :: IO ()
 main = do
-    [sushi, infile] <- getArgs
-    putStrLn $ "-- We use SuSHi: " ++ sushi
-    validExe <- isValidExecutable sushi
-    unless validExe $ die ("-- Invalid SuSHi executable: " ++ sushi)
+    -- inp <- unwrapRecord "Run SuSHi to obtain the cross sections"
+
+    -- let mdtyp = fromMaybe 2 (mtype inp)
+
+    [sushiPath, infile] <- getArgs
+    putStrLn $ "-- We use SuSHi: " ++ sushiPath
+    validExe <- isValidExecutable sushiPath
+    unless validExe $ die ("-- Invalid SuSHi executable: " ++ sushiPath)
 
     str <- T.lines <$> TIO.readFile infile
     let str' = map (replaceSinBA
@@ -40,7 +51,7 @@ main = do
 
     withFile inpF WriteMode $ \h -> mapM_ (TIO.hPutStrLn h) str'
 
-    outputStr <- readProcess sushi [inpF, outF] []
+    outputStr <- readProcess sushiPath [inpF, outF] []
     putStrLn outputStr
 
     slha <- getSLHASpec outF
@@ -70,3 +81,20 @@ isValidExecutable exe = do
     exists <- doesFileExist exe
     isExec <- executable <$> getPermissions exe
     return $ exists && isExec
+
+data InputArgs w = InputArgs
+    { sushi    :: w ::: FilePath       <?> "SuSHi executable (which sushi)"
+    , input    :: w ::: Maybe FilePath <?> "template for the input to SuSHi (input_template.in)"
+    , mtype    :: w ::: Maybe Int      <?> "model type (either 1 or 2)"
+    , mH       :: w ::: [Double]       <?> "heavy Higgs mass"
+    , mA       :: w ::: Maybe Double   <?> "CP-odd Higgs mass"
+    , mHp      :: w ::: Double         <?> "charged Higgs mass"
+    , mS       :: w ::: Maybe Double   <?> "heavy mass scale (m_A if MSSM)"
+    , tanb     :: w ::: Double         <?> "tan(beta)"
+    , cosba    :: w ::: Double         <?> "cos(beta-alpha)"
+    , stepsize :: w ::: Maybe Double   <?> "step size (default: 0.5)"
+    , output   :: w ::: Maybe String   <?> "the name of the output file"
+    } deriving Generic
+
+instance ParseRecord (InputArgs Wrapped)
+deriving instance Show (InputArgs Unwrapped)
